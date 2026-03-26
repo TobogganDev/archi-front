b5296c8e259c148ab5c8d8531d316b8bcc04d23f
TobogganDev <doretg.t@gmail.com>
2026-03-26 14:14:44 +0100

    :rocket: Enhance customer management with error boundaries and suspense queries

--- src/entities/customer/index.ts
+++ src/entities/customer/index.ts
@@ -1,4 +1,4 @@
 export type { Customer, CustomerInsert, CustomerUpdate } from "./model/customer.types";
-export { useCustomers, useCustomerById } from "./model/useCustomers";
+export { useCustomers, useSuspenseCustomers, useCustomerById } from "./model/useCustomers";
 export { createCustomer, deleteCustomer } from "./api/customer.api";
 export { useCustomerStore, selectSearchQuery, selectSetSearchQuery, selectHasActiveSearch, selectSelectedCustomerId, selectSetSelectedCustomerId, selectResetFilters } from "./model/customer.store";
